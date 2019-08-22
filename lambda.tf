134426c37a0a5f0932e93b89fb6f9f0df0dc2695
Chris Symon <chris.symon97@gmail.com>
2019-08-22 18:23:44 +1200

    updated lambda deployment

--- lambda.tf
+++ lambda.tf
@@ -2,19 +2,19 @@
 #lambda then retrieves the code from the bucket
 resource "aws_s3_bucket" "lambda_bucket" {
     bucket    = "${var.lambda_bucket}"
-    acl       = "public-read"
-    policy    = <<POLICY
-{
-    "Version":"2012-10-17",
-    "Statement":[{
-      "Sid":"AddPerm",
-      "Effect":"Allow",
-      "Principal":"*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::${var.lambda_bucket}/*"]
-    }]
-}
-POLICY
+    acl       = "private"
+#     policy    = <<POLICY
+# {
+#     "Version":"2012-10-17",
+#     "Statement":[{
+#       "Sid":"AddPerm",
+#       "Effect":"Allow",
+#       "Principal":"*",
+#       "Action":["s3:GetObject"],
+#       "Resource":["arn:aws:s3:::${var.lambda_bucket}/*"]
+#     }]
+# }
+# POLICY
 
     provisioner "local-exec" {
         when = "destroy"
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_object" "lambda_code" {
 
 #IAM role for the lambda function(s)
 resource "aws_iam_role" "lambda_role" {
-    name =  "test-example-lambda"
+    name =  "lambda_role"
     assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -71,7 +71,7 @@ resource "aws_lambda_function" "stockdata_lambda" {
     role = "${aws_iam_role.lambda_role.arn}"
 }
 
-resource "aws_lambda_function" "stocklist_lamda" {
+resource "aws_lambda_function" "stocklist_lambda" {
     function_name   = "stocklist"
 
     s3_bucket       = "${aws_s3_bucket.lambda_bucket.id}"
@@ -114,16 +114,16 @@ resource "aws_lambda_permission" "stockdata_api" {
     function_name   = "${aws_lambda_function.stockdata_lambda.arn}"
     principal       = "apigateway.amazonaws.com"
 
-    source_arn      = "${aws_api_gateway_deployment.api_deployment.execution_arn}/*/*"
+    source_arn      = "${aws_api_gateway_deployment.deployment.execution_arn}/*/*"
 }
 
 resource "aws_lambda_permission" "stocklist_api" {
     statement_id    = "AllowAPIGatewayInvoke"
     action          = "lambda:InvokeFunction"
-    function_name   = "${aws_lambda_function.stocklist_lamda.arn}"
+    function_name   = "${aws_lambda_function.stocklist_lambda.arn}"
     principal       = "apigateway.amazonaws.com"
 
-    source_arn      = "${aws_api_gateway_deployment.api_deployment.execution_arn}/*/*"
+    source_arn      = "${aws_api_gateway_deployment.deployment.execution_arn}/*/*"
 }
 
 resource "aws_lambda_permission" "subscriptions_api" {
@@ -132,7 +132,7 @@ resource "aws_lambda_permission" "subscriptions_api" {
     function_name   = "${aws_lambda_function.subscriptions_lambda.arn}"
     principal       = "apigateway.amazonaws.com"
 
-    source_arn      = "${aws_api_gateway_deployment.api_deployment.execution_arn}/*/*"
+    source_arn      = "${aws_api_gateway_deployment.deployment.execution_arn}/*/*"
 }
 
 resource "aws_lambda_permission" "subscribe_api" {
@@ -141,5 +141,5 @@ resource "aws_lambda_permission" "subscribe_api" {
     function_name   = "${aws_lambda_function.subscribe_lambda.arn}"
     principal       = "apigateway.amazonaws.com"
 
-    source_arn      = "${aws_api_gateway_deployment.api_deployment.execution_arn}/*/*"
+    source_arn      = "${aws_api_gateway_deployment.deployment.execution_arn}/*/*"
 }
